9bacce0a84ff3362e45e1395b859249a3d28be5f
kevinhuang78 <kevin_huang_iim@yahoo.com>
2023-01-27 17:11:23 +0100

    misc: add SWR

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.data);
 
 const Container = styled.header`
   padding: 25px 15px;
@@ -9,6 +12,11 @@ const Container = styled.header`
 const Universes = styled.nav``;
 
 const Header = () => (
+  /* const { data, error, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_WECASA_API_URL}/universes.json`,
+    fetcher
+  ); */
+
   <Container>
     <Link href="/">
       <Image src="/wecasa.svg" alt="Wecasa logo" width={130} height={24} />
@@ -16,5 +24,4 @@ const Header = () => (
     <Universes />
   </Container>
 );
-
 export default Header;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  serverRuntimeConfig: {
+    NEXT_PUBLIC_WECASA_API_URL: process.env.NEXT_PUBLIC_WECASA_API_URL,
+  },
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_WECASA_API_URL: process.env.NEXT_PUBLIC_WECASA_API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
